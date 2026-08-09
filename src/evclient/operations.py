a4cfcec2643d200111da7f82825d604ddbd6db21
Wannes Vantorre <vantorrewannes@gmail.com>
2026-08-09 14:23:27 +0200

    Add call_get_user operation and update register endpoint
    Signed-off-by: Wannes Vantorre <vantorrewannes@gmail.com>

--- src/evclient/operations.py
+++ src/evclient/operations.py
@@ -11,6 +11,7 @@ if TYPE_CHECKING:
 REGISTER_USER_ENDPOINT = "/user/register"
 REGISTER_USER_ID_ENDPOINT = "/user/register/{user_id}"
 DELETE_USER_ENDPOINT = "/user/{user_id}"
+GET_USER_ENDPOINT = "/user/{user_id}"
 ARCHIVE_STORE_FILE = ".ev"
 
 
@@ -18,14 +19,14 @@ async def get_archive_store(workspace_path: Path) -> Store:
     return FileStore(workspace_path / ARCHIVE_STORE_FILE)
 
 
-async def call_register_user(archive_url: str) -> User:
+async def call_register_user(archive_url: str) -> UserId:
     url = archive_url + REGISTER_USER_ENDPOINT
     async with (
         aiohttp.ClientSession() as session,
         session.post(url) as response,
     ):
         response.raise_for_status()
-        return User(**await response.json())
+        return UserId(await response.json())
 
 
 async def call_register_user_id(archive_url: str, user_id: UserId) -> None:
@@ -46,12 +47,21 @@ async def call_delete_user_id(archive_url: str, user_id: UserId) -> None:
         response.raise_for_status()
 
 
+async def call_get_user(archive_url: str, user_id: UserId) -> User:
+    url = archive_url + GET_USER_ENDPOINT.format(user_id=user_id)
+    async with (
+        aiohttp.ClientSession() as session,
+        session.get(url) as response,
+    ):
+        response.raise_for_status()
+        return User(**await response.json())
+
+
 async def register_command(archive_url: str, user_id: UserId | None = None) -> UserId:
     if user_id:
         await call_register_user_id(archive_url, user_id)
         return user_id
-    user = await call_register_user(archive_url)
-    return user.id
+    return await call_register_user(archive_url)
 
 
 async def unregister_command(archive_url: str, user_id: UserId) -> None:
