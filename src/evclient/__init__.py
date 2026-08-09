a864128ae39c3ffe8cd7cd8fd9aa37cfddc43c8b
Wannes Vantorre <vantorrewannes@gmail.com>
2026-08-09 13:32:47 +0200

    Add aiohttp client operations for user management
    Signed-off-by: Wannes Vantorre <vantorrewannes@gmail.com>

--- src/evclient/__init__.py
+++ src/evclient/__init__.py
@@ -1,2 +1,16 @@
+import asyncio
+
+from evclient.operations import call_delete_user_id, call_register_user_id
+from evclient.types import User
+
+
+async def run() -> None:
+    uri = "http://127.0.0.1:8000"
+    user = User.random()
+    user_id = await call_register_user_id(uri, user.id)
+    user_id = await call_delete_user_id(uri, user.id)
+    print(user_id)  # noqa: T201
+
+
 def main() -> None:
-    print("Hello from evclient!")
+    asyncio.run(run())
---
+++ src/evclient/operations.py
@@ -0,0 +1,35 @@
+import aiohttp
+
+from evclient.types import User, UserId
+
+REGISTER_USER_ENDPOINT = "/user/register"
+REGISTER_USER_ID_ENDPOINT = "/user/register/{user_id}"
+DELETE_USER_ENDPOINT = "/user/{user_id}"
+
+
+async def call_register_user(archive_url: str) -> User:
+    url = archive_url + REGISTER_USER_ENDPOINT
+    async with (
+        aiohttp.ClientSession() as session,
+        session.post(url) as response,
+    ):
+        response.raise_for_status()
+        return User(**await response.json())
+
+
+async def call_register_user_id(archive_url: str, user_id: UserId) -> None:
+    url = archive_url + REGISTER_USER_ID_ENDPOINT.format(user_id=user_id)
+    async with (
+        aiohttp.ClientSession() as session,
+        session.post(url) as response,
+    ):
+        response.raise_for_status()
+
+
+async def call_delete_user_id(archive_url: str, user_id: UserId) -> None:
+    url = archive_url + DELETE_USER_ENDPOINT.format(user_id=user_id)
+    async with (
+        aiohttp.ClientSession() as session,
+        session.delete(url) as response,
+    ):
+        response.raise_for_status()
